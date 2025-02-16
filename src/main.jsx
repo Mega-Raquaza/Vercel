1baa2b35acc23c08e1e79d2e9be02c4dce01c47b
07 <animelover.weebs.07@gmail.com>
2025-02-16 08:48:45 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { AuthProvider } from "./Context/AuthContext"; // ✅ Correct import
+import { AuthProvider } from "./context/AuthContext"; // ✅ Correct import
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
     <App />
